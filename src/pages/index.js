fc4e2275251669768870add15a80661f4d311d5c
M. Hanif Azhary <hanif.hz@gmail.com>
2021-04-13 20:01:55 +0000

    npm package cleaning for better experience

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,42 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <Seo
+      keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
+      title="Home"
+    />
+
+    <section className="text-center">
+      <img
+        alt="Cat and human sitting on a couch"
+        className="block w-1/2 mx-auto mb-8"
+        src={catAndHumanIllustration}
+      />
+
+      <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
+        Hey there! Welcome to your first Gatsby site.
+      </h2>
+
+      <p className="leading-loose">
+        This is a barebones starter for Gatsby styled using{` `}
+        <a
+          className="font-bold text-gray-900 no-underline"
+          href="https://tailwindcss.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tailwind CSS
+        </a>
+        , a utility-first CSS framework.
+      </p>
+    </section>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
